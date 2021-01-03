30f46e6ab611938f5858b19c7c7ff1ba9b1217df
jensvde <jens.vandeneynde@telenet.be>
2021-01-03 06:37:19 +0100

    v

--- Winkeltje/Controllers/DatabaseController.cs
+++ Winkeltje/Controllers/DatabaseController.cs
@@ -85,7 +85,7 @@ namespace Winkeltje.Controllers
         [HttpGet("/Database/Reboot/")]
         public async Task<IActionResult> RebootAsync()
         {
-            await $"/usr/sbin/shutdown -ar now".Bash();
+            await $"/usr/bin/shutdown -ar now".Bash();
                 return RedirectToAction("Index", "Home");
         }
         public async Task<IActionResult> UploadDbUsers(IFormFile formFile)
