dbf7c867e8a0a820616c6686b32f50a84f776c56
jensvde <jens.vandeneynde@telenet.be>
2021-01-03 04:59:22 +0100

    ddd

--- Winkeltje/Controllers/DatabaseController.cs
+++ Winkeltje/Controllers/DatabaseController.cs
@@ -76,7 +76,7 @@ namespace Winkeltje.Controllers
         public async Task<ActionResult> DbExport(string Database)
         {
             await $"/home/{Environment.UserName}/mysql.sh --export {Database} /home/{Environment.UserName}/{Database}.db".Bash();
-            string filePath = Environment.UserName + "/" + Database + ".db";
+            string filePath = "/home/" + Environment.UserName + "/" + Database + ".db";
             string fileName = Database + ".db";
             byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
             return File(fileBytes, "application/force-download", fileName);
